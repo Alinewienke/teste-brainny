32dea8710469ed430d58d66f5ca6e25573bac70b
Aline Wienke <wienkealine@gmail.com>
2023-08-30 14:01:54 -0300

    :sparkles: cria estrutura da pagina meus registros

--- src/pages/MyRegistries.jsx
+++ src/pages/MyRegistries.jsx
@@ -1,7 +1,36 @@
+import { Box, Flex } from '@chakra-ui/react';
+
+import Sidebar from '../components/Sidebar/Sidebar';
+import WorkerCard from '../components/WorkerCard/WorkerCard';
+import WorkerHeader from '../components/WorkerHeader/WorkerHeader';
+import RegistrationModal from '../components/RegistrationModal/RegistrationModal';
+
 function MyRegistries() {
   return (
     <>
-      <h1>My Registries Page</h1>
+      <Box
+        width='100%'
+        height='100vh'
+        bg='#F2F2F2'
+      >
+        <Flex>
+          <Sidebar pageName='My Registries' />
+          <Box
+            width='100%'
+            px='6'
+            py='10'
+          >
+            <RegistrationModal />
+            <WorkerHeader />
+            <WorkerCard
+              workerName='João Silva'
+              workerNumber='001'
+              date='12/10/19'
+              hour='18:30h'
+            />
+          </Box>
+        </Flex>
+      </Box>
     </>
   );
 }
