1f7b9b64e0d7c2b25f60a46d8cea5f97a3b485b6
Aline Wienke <wienkealine@gmail.com>
2023-08-30 13:57:40 -0300

    :sparkles: adiona modal de registro de ponto

---
+++ src/components/RegistrationModal/RegistrationModal.jsx
@@ -0,0 +1,102 @@
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton } from '@chakra-ui/react';
+import { Button, useDisclosure, Flex } from '@chakra-ui/react';
+import { Heading } from '@chakra-ui/react';
+import { Image } from '@chakra-ui/react';
+import clock from '../../assets/clock.png';
+import { Text } from '@chakra-ui/react';
+
+function RegistrationModal() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const date = new Date();
+
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+
+  let currentDate = `${day}/${month}/${year}`;
+  let currentHour = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+  return (
+    <>
+      <Button
+        color='white'
+        bg='principal'
+        fontWeight='400'
+        onClick={onOpen}
+        w='200px'
+        fontSize='16px'
+        mb='8'
+      >
+        Registrar ponto
+      </Button>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        size='sm'
+      >
+        <ModalOverlay />
+        <ModalContent py='10'>
+          <ModalHeader>
+            <Heading
+              fontSize='20px'
+              textAlign='center'
+            >
+              Registrar novo ponto
+            </Heading>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Flex
+              align='center'
+              justify='center'
+              flexDirection='column'
+            >
+              <Image
+                src={clock}
+                alt='clock'
+                mb='2'
+              />
+              <Text
+                fontSize='30px'
+                color='principal'
+                fontWeight='700'
+              >
+                {currentHour}
+              </Text>
+              <Text
+                fontSize='16px'
+                color='secondary'
+                mb='6'
+              >
+                {currentDate}
+              </Text>
+
+              <Button
+                w='200px'
+                onClick={onClose}
+                color='white'
+                bg='principal'
+                fontWeight='400'
+                mb='2'
+              >
+                Bater ponto
+              </Button>
+              <Button
+                colorScheme='teal'
+                variant='outline'
+                color='principal'
+                fontWeight='400'
+                w='200px'
+              >
+                Cancelar
+              </Button>
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+export default RegistrationModal;
